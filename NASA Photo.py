7152f9d7cb9aa1e05b89d5c5de3db191fe0e7d10
Arya Zhao <lz2650@columbia.edu>
2020-02-20 02:36:29 -0500

    clean metadata, generate csv and upload to cloud

--- NASA Photo.py
+++ NASA Photo.py
@@ -19,20 +19,20 @@ def download_photo_by_date(date):
           +'&api_key=' + nasa_key
     res = requests.get(url).json()
     photos_list = res['photos']
+
+    index_list = []
+
     for photo_info in photos_list:
         url = photo_info['img_src']
         filename = url.split('/')[-1]
+        index_list.append(filename.split('.')[0])
         filepath = './images/' + filename
         urllib.request.urlretrieve(url, filepath)
-        # print(filename, ' is downloaded')
+        print(filename, ' is downloaded')
+    create_metadata_csv(photos_list, index_list)
 
 
 def upload_blob(bucket_name, source_file_directory):
-    """Uploads a file to the bucket."""
-    # bucket_name = "your-bucket-name"
-    # source_file_name = "local/path/to/file"
-    # destination_blob_name = "storage-object-name"
-
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     for filename in os.listdir(source_file_directory):
@@ -41,7 +41,38 @@ def upload_blob(bucket_name, source_file_directory):
         blob.upload_from_filename(filepath)
         print(filename, ' is uploaded')
 
+def create_metadata_csv(list_of_dict, index_list):
+    clean_list_of_dict = []
+    for img in list_of_dict:
+        clean_dict = {}
+        clean_dict['id'] = img['id']
+        camera = img['camera']
+        for key in camera.keys():
+            clean_dict["camera "+key] = camera[key]
+
+        rover = img['rover']
+        for key in rover.keys():
+            if key == 'cameras':
+                cameras = ''
+                for data_dict in rover[key]:
+                    cameras = cameras + data_dict['name']+' ( '+data_dict['full_name']+' ), '
+                clean_dict["rover "+key] = cameras[0:-2]
+            else:
+                clean_dict["rover  " + key] = rover[key]
+        clean_dict['sol'] = img['sol']
+        clean_dict['earth_date'] = img['earth_date']
+        clean_dict['img_src'] = img['img_src']
+        clean_list_of_dict.append(clean_dict)
+
+
+    df = pd.DataFrame(clean_list_of_dict, index=index_list)
+    df.index.name = 'filename'
+    df.to_csv(r'./metadata.csv')
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('nasa-photos')
+    blob = bucket.blob('metadata.csv')
+    blob.upload_from_filename('./metadata.csv')
 
 if __name__ == '__main__':
-    # download_photo_by_date('2016-2-2');
+    download_photo_by_date('2016-2-2');
     upload_blob('nasa-photos', 'images')
