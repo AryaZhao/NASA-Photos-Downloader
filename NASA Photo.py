7e1e18e0c07bea551904ebfb75ae338f48ed21fb
Arya Zhao <lz2650@columbia.edu>
2020-02-20 00:42:30 -0500

    upload photos to google cloud storage using python

--- NASA Photo.py
+++ NASA Photo.py
@@ -5,12 +5,18 @@ import requests
 import json
 import pandas as pd
 import urllib.request
+from google.cloud import storage
+from dotenv import load_dotenv
+import os
+import os.path
 
+load_dotenv()
+google_app_credential = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+nasa_key = os.getenv("NASA_API_KEY")
 
 def download_photo_by_date(date):
-    my_key = 'hum8hHccg2KFrhKW5fFCJ0y9M8SVfcye3CS5SnJ4'
     url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='+date\
-          +'&api_key=' + my_key
+          +'&api_key=' + nasa_key
     res = requests.get(url).json()
     photos_list = res['photos']
     for photo_info in photos_list:
@@ -18,7 +24,24 @@ def download_photo_by_date(date):
         filename = url.split('/')[-1]
         filepath = './images/' + filename
         urllib.request.urlretrieve(url, filepath)
-        # print('downloading photo '+ filename)
+        # print(filename, ' is downloaded')
+
+
+def upload_blob(bucket_name, source_file_directory):
+    """Uploads a file to the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    for filename in os.listdir(source_file_directory):
+        filepath = './images/'+filename
+        blob = bucket.blob(filename)
+        blob.upload_from_filename(filepath)
+        print(filename, ' is uploaded')
+
 
 if __name__ == '__main__':
-    download_photo_by_date('2016-2-2');
+    # download_photo_by_date('2016-2-2');
+    upload_blob('nasa-photos', 'images')
